536c5ac5586fe998d12e358c677131a3388ac784
prakash <54275178+prakashbhagat@users.noreply.github.com>
2022-01-08 14:47:04 +0530

    Update environment.prod.ts

--- user/src/environments/environment.prod.ts
+++ user/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-SERVER_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/api',
-  	IMAGE_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/',
+SERVER_URL : '',
+  	IMAGE_URL : '',
 };
