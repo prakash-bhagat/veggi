0468268f1d8454769a9a0e35eefa85967cbaa2e3
prakash <54275178+prakashbhagat@users.noreply.github.com>
2022-01-08 14:49:52 +0530

    Update environment.ts

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -6,19 +6,9 @@
 
 export const environment = {
   production: false,
-   SERVER_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/api',
-  IMAGE_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/',
+   SERVER_URL : '',
+  IMAGE_URL : '',
   
-   firebase : {
-    apiKey: "AIzaSyCIgF0p5HldPhaakG_rluiaj0ZTanRQaSk",
-    authDomain: "homease-5858.firebaseapp.com",
-    databaseURL: "https://homease-5858.firebaseio.com",
-    projectId: "homease-5858",
-    storageBucket: "homease-5858.appspot.com",
-    messagingSenderId: "364586467493",
-    appId: "1:364586467493:web:d93c98be8c7599439f0d4c",
-    measurementId: "G-20XRCGWCKG"
-  }
 };
 
 /*
