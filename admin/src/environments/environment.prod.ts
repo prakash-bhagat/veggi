977f00874775f650a35cff0616e15486afc3bee4
prakash <54275178+prakashbhagat@users.noreply.github.com>
2022-01-08 14:50:16 +0530

    Update environment.prod.ts

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -2,16 +2,6 @@
 
 export const environment = {
   production: true,
-  SERVER_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/api',
-  IMAGE_URL : 'http://ec2-65-0-91-147.ap-south-1.compute.amazonaws.com/',
-  firebase : {
-    apiKey: "AIzaSyCIgF0p5HldPhaakG_rluiaj0ZTanRQaSk",
-    authDomain: "homease-5858.firebaseapp.com",
-    databaseURL: "https://homease-5858.firebaseio.com",
-    projectId: "homease-5858",
-    storageBucket: "homease-5858.appspot.com",
-    messagingSenderId: "364586467493",
-    appId: "1:364586467493:web:d93c98be8c7599439f0d4c",
-    measurementId: "G-20XRCGWCKG"
-  }
+  SERVER_URL : '',
+  IMAGE_URL : '',
 };
